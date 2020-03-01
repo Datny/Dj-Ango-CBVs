e8b016d06e6b80b2988e4b9fc7caadbd428a5fd1
Robert P <robert.przydatek.cl@gmail.com>
2020-03-01 18:23:54 +0100

    override form_valid for creating hall

--- learncbvs-proj/core/views.py
+++ learncbvs-proj/core/views.py
@@ -3,6 +3,8 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from . models import Hall
+
+
 # Create your views here.
 def home(request):
     return render(request,"core/home.html")
@@ -13,12 +15,15 @@ class CreateHall(generic.CreateView):
     template_name = 'core/create_hall.html'
     success_url = reverse_lazy('home')
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        super().form_valid(form)
+        return redirect('home')
+
+
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
-    def form_valid(self, form):
-        form.instance.user = self.request.user
-        super(CreateHall, self).form_valid(form)
-        return redirect('home')
+
