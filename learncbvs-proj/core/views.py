d20f85333009f2a6f9032be779fbdfb0eba58fcd
Robert P <robert.przydatek.cl@gmail.com>
2020-03-02 16:21:48 +0100

    delete cbv fin

--- learncbvs-proj/core/views.py
+++ learncbvs-proj/core/views.py
@@ -2,18 +2,55 @@ from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 from . models import Hall
+from django.shortcuts import get_object_or_404
+
+
+
+
+
+
 
 
 # Create your views here.
 def home(request):
     return render(request,"core/home.html")
 
+def dashboard(request):
+    return render(request,"core/dashboard.html")
+
+class DetailHall(generic.DetailView):
+    model = Hall
+    template_name = "core/detail_hall.html"
+
+
+class UpdateHall(generic.UpdateView):
+    model = Hall
+    template_name = "core/update_hall.html"
+    fields = ['title']
+    success_url = reverse_lazy('dashboard')
+
+class DeleteHall(generic.DeleteView):
+    model = Hall
+    success_url = reverse_lazy("dashboard")
+    template_name = 'core/delete_hall.html'
+
+
+    def delete(self, request, *args, **kwargs):
+       self.object = self.get_object()
+       if self.object.user == request.user:
+          self.object.delete()
+          return redirect(self.get_success_url())
+       else:
+          return redirect('login')
+
+
 class CreateHall(generic.CreateView):
     model = Hall
     fields = ['title']
     template_name = 'core/create_hall.html'
-    success_url = reverse_lazy('home')
+    success_url = reverse_lazy('dashboard')
 
     def form_valid(self, form):
         form.instance.user = self.request.user
@@ -26,4 +63,9 @@ class SignUp(generic.CreateView):
     success_url = reverse_lazy('home')
     template_name = 'registration/signup.html'
 
-
+    def form_valid(self, form):
+        view = super().form_valid(form)
+        username, password = form.cleaned_data.get('username'), form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(self.request, user)
+        return view
